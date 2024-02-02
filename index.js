cb37a2f9328ae957dae67e101e7d04bacb0d82e4
doni-zete <donizete112@gmail.com>
2024-02-01 21:41:49 -0300

    feat: conectando o servidor ao database

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const app = express()
+const connectToDatabase = require('./src/databases/database')
 
 port = 5000
 
-app.get("/", (req, res) => {
-    res.send({
-        message: "Bem vindo a nossa loja ",
-    });
-});
+connectToDatabase()
+    .then((connection) => {
 
-app.listen(port, () => {
-    console.log(`Servidor rodando em: http://localhost:${port}`);
-});
+        console.log('Conexão com o banco de dados estabelecida')
+
+
+        app.get('/', (req, res) => {
+            res.json({
+                message: 'Bem-vindo à nossa loja',
+            })
+        })
+
+        // Iniciar o servidor Express
+        app.listen(port, () => {
+            console.log(`Servidor rodando em: http://localhost:${port}`)
+        })
+    })
+    .catch((error) => {
+        console.error('Erro na inicialização do servidor:', error)
+    })
