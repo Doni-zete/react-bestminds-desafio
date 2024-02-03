3c882b75dda6e01634d01bd7cd9ee271ab074c14
doni-zete <donizete112@gmail.com>
2024-02-02 21:49:28 -0300

    feat: Creating controller to select products

--- backend-crud-mysql/src/controllers/produtoController.js
+++ backend-crud-mysql/src/controllers/produtoController.js
@@ -1,23 +1,18 @@
-// produtoController.js
-import connectToDatabase from '../databases/database.js';
-
-const produtoController = async (req, res) => {
+import { connectToDatabase } from '../databases/database.js';
+ const selectProdutoController = async (req, res) => {
     try {
-        // Conecta ao banco de dados
         const connection = await connectToDatabase();
-
-        // Realiza a consulta
         const [selecionaProdutos] = await connection.query('SELECT * FROM produtos');
-
-        // Fecha a conexão
         connection.release();
 
-        // Envia a resposta para o cliente
         res.json({ produtos: selecionaProdutos });
+        
     } catch (error) {
         console.error('Erro na consulta ao banco de dados:', error.message);
         res.status(500).json({ error: `Erro interno do servidor ${error}` });
     }
 };
 
-export default produtoController;
+const produtoController = { selectProdutoController }
+
+export default produtoController
