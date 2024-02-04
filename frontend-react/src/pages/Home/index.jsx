4013c78688fc5a09465069f3196467e6b647ff61
doni-zete <donizete112@gmail.com>
2024-02-03 22:21:12 -0300

    feat: Adding home route

---
+++ frontend-react/src/pages/Home/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Footer from './../../componets/Footer'
+import NavMenu from '../../componets/NavMenu'
+
+const Home = () => {
+
+ return (
+  <>
+   <NavMenu />
+         <section>
+             
+    </section>
+
+    <Footer />
+   </>
+
+ )
+}
+
+export default Home
