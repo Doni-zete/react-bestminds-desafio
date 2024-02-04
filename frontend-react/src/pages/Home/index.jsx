55d6500dfb0712d4245167efd31171c4d35284fa
doni-zete <donizete112@gmail.com>
2024-02-04 11:18:39 -0300

    feat: Adding home page to application home page with options to choose clothes

--- frontend-react/src/pages/Home/index.jsx
+++ frontend-react/src/pages/Home/index.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import ProdutoCamisaGola from '../../componets/ProdutoCamisaGola'
+import ProdutoBermuda from '../../componets/ProdutoBermuda'
 import Footer from './../../componets/Footer'
-import NavMenu from '../../componets/NavMenu'
+
 
 const Home = () => {
+ const [categoriaTab, setCategoriaTab] = useState('Camisa')
+
+ const handleCategoriaChange = (categoria) => {
+  setCategoriaTab(categoria)
+ }
+
+ const handleTabClick = (categoria, event) => {
+  event.preventDefault() 
+  handleCategoriaChange(categoria)
+ }
 
  return (
   <>
-   <NavMenu />
-         <section>
-             
-    </section>
+   <section className="header-banner flex justify-center items-center h-screen">
+    <h1 className="text-3xl md:text-5xl lg:text-5xl font-semibold text-red-400 -mt-32 md:-mt-48 lg:-mt-60 text-center">
+     Conjunto Sportswear de Treino Masculino!
+    </h1>
+   </section>
+   <div></div>
+   <div className="flex items-center justify-center space-x-6 py-12">
+    <p
+     className={
+      categoriaTab === 'Camisa'
+       ? 'active-menu-tab bg-primary'
+       : 'menu-tab'
+     }
+     onClick={(e) => handleTabClick('Camisa', e)}
+    >
+     Camisa
+    </p>
+    <p
+     className={
+      categoriaTab === 'Bermuda' ? 'active-menu-tab bg-primary' : 'menu-tab'
+     }
+     onClick={(e) => handleTabClick('Bermuda', e)}
+    >
+     Bermuda
+    </p>
+   </div>
+   {categoriaTab === 'Camisa' && <ProdutoCamisaGola />}
+   {categoriaTab === 'Bermuda' && <ProdutoBermuda />}
 
-    <Footer />
-   </>
-
+   <Footer />
+  </>
  )
 }
 
