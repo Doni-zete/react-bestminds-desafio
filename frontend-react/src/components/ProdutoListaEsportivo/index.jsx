122d8f81aed097c51b31e1f907ad102155438dbb
doni-zete <donizete112@gmail.com>
2024-02-06 07:24:00 -0300

    fix: Removing the underscore to find the id and correcting import

--- frontend-react/src/components/ProdutoListaEsportivo/index.jsx
+++ frontend-react/src/components/ProdutoListaEsportivo/index.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import ProdutosEsportivos from '../../components/ProdutosEsportivos'
-import { findAllProducts } from '../../services/produtoCamisaService'
+import { findAllProducts } from '../../services/produtoEsportivoService'
 
 const ProdutoListaEsportivo = () => {
- const [categoriaTab, setCategoriaTab] = useState('Produto')
+ const [categoriaTab] = useState('Produto')
  const [products, setProducts] = useState([])
 
  useEffect(() => {
   getAllProducts()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  
  }, [categoriaTab])
  const getAllProducts = async () => {
   try {
@@ -31,8 +31,8 @@ const ProdutoListaEsportivo = () => {
  return (
   <section className="my-12 max-w-screen-xl mx-auto px-3">
    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-12">
-    {products.map((product) => (
-     <ProdutosEsportivos key={product._id} product={product} />
+    {products.map((product, index) => (
+     <ProdutosEsportivos key={index} product={product} />
     ))}
    </div>
   </section>
