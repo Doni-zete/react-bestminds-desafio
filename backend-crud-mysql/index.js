95beb2b34258284364380a9d302c9edfb20b7b3c
doni-zete <donizete112@gmail.com>
2024-02-06 09:18:09 -0300

    fix: Space correction

--- backend-crud-mysql/index.js
+++ backend-crud-mysql/index.js
@@ -1,42 +1,37 @@
 import express from 'express'
 const app = express()
-import { connectToDatabase } from './src/databases/database.js';
+import { connectToDatabase } from './src/databases/database.js'
 import produtoRoute from './src/routes/produtosRoutes.js'
 
-
 import cors from 'cors'
-    
-app.use(express.json());
+
+app.use(express.json())
 const port = 3000
 
-
 app.use(
-    cors({
-        origin: ['*'],
-        origin: ['http://localhost:3001', 'http://localhost:3002'],
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    })
+ cors({
+  origin: ['*'],
+  origin: ['http://localhost:3001', 'http://localhost:3002'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+ })
 )
 
 app.use('/produto', produtoRoute)
 
 connectToDatabase()
-    .then((connection) => {
+ .then((connection) => {
+  console.log('Conexão com o banco de dados estabelecida')
 
-        console.log('Conexão com o banco de dados estabelecida')
+  app.get('/', (req, res) => {
+   res.json({
+       message: 'Bem-vindo à nossa loja, Nunes Sport!',
+   })
+  })
 
-
-        app.get('/', (req, res) => {
-            res.json({
-                message: 'Bem-vindo à nossa loja',
-            })
-        })
-
-        // Iniciar o servidor Express
-        app.listen(port, () => {
-            console.log(`Servidor rodando em: http://localhost:${port}`)
-        })
-    })
-    .catch((error) => {
-        console.error('Erro na inicialização do servidor:', error)
-    })
+  app.listen(port, () => {
+   console.log(`Servidor rodando em: http://localhost:${port}`)
+  })
+ })
+ .catch((error) => {
+  console.error('Erro na inicialização do servidor:', error)
+ })
