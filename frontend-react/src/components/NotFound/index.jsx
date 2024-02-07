9fb22970728234b364f2af98abda9fc10d5ad28c
doni-zete <donizete112@gmail.com>
2024-02-07 09:23:34 -0300

    feat: Adding footer to notfound page

--- frontend-react/src/components/NotFound/index.jsx
+++ frontend-react/src/components/NotFound/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import Footer from './../../components/Footer'
 const NotFound = () => {
-  return <div className="h-screen banner-not-found"></div>;
+  return (
+   <div>
+    <div className="h-screen banner-not-found"></div>
+    <Footer />
+   </div>
+  )
+ 
 };
 
 export default NotFound;
